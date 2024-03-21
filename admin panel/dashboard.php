04faa049cbb7ef44c0566d2ff8677fc1378dc90b
Sohesh Maharjan <soheshmaharjan.077@kathford.edu.np>
2024-03-21 22:23:49 +0545

    login page added

---
+++ admin panel/dashboard.php
@@ -0,0 +1,35 @@
+<?php
+    include '../componets/connect.php';
+
+    if(isset($_COOKIE['seller_id'])){
+        $seller_id = $_COOKIE['seller_id'];
+    }else{
+        $seller_id = '';
+        header('location:login.php');
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blue Sky Summer - seller registration page</title>
+    <link rel="stylesheet" type="text/css" href="../css/admin_style.css">
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+</head>
+<body> 
+    <div class="main-container">
+        <?php include '../componets/admin_header.php'; ?>
+    </div>
+  
+
+
+
+    <!-- sweetalert cdn link -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+    <!-- custom js link -->
+    <script src="../js/script.js"></script>
+    <?php include '../componets/alert.php'; ?>  
+</body>
+</html>
