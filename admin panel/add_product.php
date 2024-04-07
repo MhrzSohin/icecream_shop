8645a83c919c577080837a6011095010ff048b5b
Sohesh Maharjan <soheshmaharjan.077@kathford.edu.np>
2024-04-07 22:46:18 +0545

    view product page started admin can add product and can save to draft

--- admin panel/add_product.php
+++ admin panel/add_product.php
@@ -52,13 +52,61 @@
             $success_msg[] = 'product inserted successfully';
         }
     }
+    //add products in database
+        if (isset($_POST['draft'])) {
+
+            $id = unique_id();
+            $name = $_POST['name'];
+            $name = filter_var($name, FILTER_SANITIZE_STRING);
+
+            $price = $_POST['price'];
+            $price = filter_var($price, FILTER_SANITIZE_NUMBER_FLOAT);
+            
+            $description = $_POST['description'];
+            $description = filter_var($description, FILTER_SANITIZE_STRING);
+
+            $stock = $_POST['stock'];
+            $stock = filter_var($stock, FILTER_SANITIZE_NUMBER_FLOAT);
+            $status = 'deactive';
+
+            $image = $_FILES['image']['name'];
+            $image = filter_var($image, FILTER_SANITIZE_STRING);
+            $image_size = $_FILES['image']['size'];
+            $image_tmp_name = $_FILES['image']['tmp_name'];
+            $image_folder = '../uploaded_files/'.$image;
+
+            $select_image = $conn->prepare("SELECT * FROM `products` WHERE image = ? AND seller_id = ?");
+            $select_image->execute([$image,$seller_id]);
+
+            if(isset($image)) {
+                if($select_image->rowCount() > 0 ){
+                    $warning_msg[] = 'image name repeated';
+                }elseif($image_size > 20000000){
+                    $warning_msg[] = 'image size too large';
+                }else{
+                    move_uploaded_file($image_tmp_name,$image_folder);
+                }
+            }else{
+                $image = '';
+            }
+            if($select_image->rowCount() > 0 AND $image != ''){
+                $warning_msg[] = 'please rename your image';
+            }else{
+                $insert_product = $conn->prepare("INSERT INTO `products`(id, seller_id, name, price, image, stock, product_details, status) VALUES(?,?,?,?,?,?,?,?)");
+                $insert_product->execute([$id, $seller_id, $name, $price, $image, $stock, $description, $status]);
+                $success_msg[] = 'product added to draft successfully';
+            }
+        }
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blue Sky Summer - Admin Dashboard page</title>
+    <title>Blue Sky Summer - Admin Add product page</title>
     <link rel="stylesheet" type="text/css" href="../css/admin_style.css">
     <!-- font awesome cdn link  -->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
@@ -95,7 +143,7 @@
                     </div>
                     <div class="flex-btn">
                          <input type="submit" name="publish" value="add product" class="btn">
-                         <input type="submit" name="darft" value="save as draft" class="btn">
+                         <input type="submit" name="draft" value="save as draft" class="btn">
                     </div>
                 </form>
             </div>
---
+++ admin panel/view_product.php
@@ -0,0 +1,73 @@
+<?php
+
+    include '../componets/connect.php';
+    if(isset($_COOKIE['seller_id'])){
+        $seller_id = $_COOKIE['seller_id'];
+    }else{
+        $seller_id = '';
+        header('location:login.php');
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blue Sky Summer - Admin Dashboard page</title>
+    <link rel="stylesheet" type="text/css" href="../css/admin_style.css">
+    <!-- font awesome cdn link  -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body> 
+    <div class="main-container">
+        <?php include '../componets/admin_header.php'; ?>
+        <section class="dashboard">
+            <div class="heading">
+                <h1>dashboard</h1>
+                <img src="../image/separator-img.png" alt="">
+            </div>
+                <div class="box-container">
+                    <?php
+                        $select_products = $conn->prepare("SELECT * FROM `products` WHERE  seller_id = ?");
+                        $select_products -> execute([$seller_id]);
+                        if($select_products->rowCount() > 0){
+                            while ($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)){
+                    ?>
+                    <form action="" method="post" class="box">
+                        <input type="hidden" name="product_id" value="<?= $fetch_products['id']; ?>">
+                        <?php if($fetch_products ['image'] != ''){ ?>
+                            <img src="../uploaded_files/<?= $fetch_products['image']; ?>" >
+                        <?php } ?>    
+                    </form>            
+                    <?php           
+                            }
+                        }else{
+                            echo '
+                            <div class="empty">
+                            <p>no products added yet!! <br> <a href="add_product.php" class="btn" style="margin-top:1.5rem">add products</a></p>
+                            </div>
+                      
+
+                            ';
+                        }
+                    ?>    
+                </div>
+        </section>
+    </div>
+    <div class="empty">
+        <p>no products added yet!! <br> <a href="add_product.php" class="btn" style="margin-top:1.5rem">add products</a></p>
+    </div>
+  
+
+ 
+  
+
+
+
+    <!-- sweetalert cdn link -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+    <!-- custom js link -->
+    <script src="../js/admin_script.js"></script>
+    <?php include '../componets/alert.php'; ?>  
+</body>
+</html>
